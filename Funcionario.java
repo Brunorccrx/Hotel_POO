2d242f1b8493b50c8043dc70ff4f2a1dfed357e8
felipey2010 <mahamaphilip@yahoo.com>
2016-12-12 11:41:21 -0400

    Add files via upload

--- Funcionario.java
+++ Funcionario.java
@@ -1,7 +1,9 @@
+
 package sistemadehotel;
 
+
 public abstract class Funcionario {
-	// Dados Pessoais
+    // Dados Pessoais
 	private int ID;
 	private String nome;
 	private String CPF;
@@ -20,6 +22,125 @@ public abstract class Funcionario {
 	//Autenticação de usuário no Sistema
 	private String login;
 	private String senha;
-	
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getNumTelefone() {
+        return numTelefone;
+    }
+
+    public void setNumTelefone(String numTelefone) {
+        this.numTelefone = numTelefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeCasa() {
+        return nomeCasa;
+    }
+
+    public void setNomeCasa(String nomeCasa) {
+        this.nomeCasa = nomeCasa;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 	
 }
