679a95a59160dce1a60886087d54fa0949ab9e7a
Gabriel Rodrigues Machado <grmachado12@hotmail.com>
2016-12-12 10:49:16 -0400

    Add files via upload

---
+++ Funcionario.java
@@ -0,0 +1,21 @@
+package Hotel;
+
+public abstract class Funcionario {
+	//
+	String nome;
+	String cpf;
+	String rg;
+	String numTelefone;
+	//Endereço
+	String rua;
+	String bairro;
+	String numeCasa;
+	String cidade;
+	String estado;
+	//Data de Nascimento
+	int dia;
+	int mes;
+	int ano;
+	
+	
+}
