bd21440bc0af3033070ea544f877cbdbacc2abfd
felipey2010 <mahamaphilip@yahoo.com>
2016-12-12 10:35:14 -0400

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadehotel;
+
+/**
+ *
+ * @author FELIPEY
+ */
+public class Cliente {
+    String nome;
+    String endereco;
+    String contato;
+    String email;
+    String nationalidade;
+    String RG;
+    String CPF;
+    String passaporte;
+    String data_de_nascimento;
+}
