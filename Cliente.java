35f535cf6311ee04548ac242b530d5d99d2e268a
felipey2010 <mahamaphilip@yahoo.com>
2016-12-12 11:13:50 -0400

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -5,6 +5,7 @@
  */
 package sistemadehotel;
 
+import java.util.Scanner;
 /**
  *
  * @author FELIPEY
@@ -19,4 +20,31 @@ public class Cliente {
     private String CPF;
     private String passaporte;
     private String data_de_nascimento;
+    
+    Scanner input = new Scanner(System.in);
+    
+    public void insereCliente(){
+        System.out.print("Nome: ");
+        this.nome = input.nextLine();
+        System.out.print("Endereço: ");
+        this.endereco = input.nextLine();
+        System.out.print("Telefone: ");
+        this.contato = input.nextLine();
+        System.out.print("E-mail: ");
+        this.email = input.nextLine();
+        System.out.print("Nationalidade: ");
+        this.nationalidade = input.nextLine();
+        if((this.nationalidade).equals("Br")){
+            System.out.print("RG: ");
+            this.RG = input.nextLine();
+            System.out.print("CPF: ");
+            this.CPF = input.nextLine();
+        }
+        else{
+            System.out.print("Passaporte: ");
+            this.passaporte = input.nextLine();
+        }
+        System.out.print("Data De Nascimento(dd/mm/yyyy): ");
+        this.data_de_nascimento = input.nextLine();
+    }
 }
