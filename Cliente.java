f589b367be1c6f782420a85aa574a22055d57a5b
felipey2010 <mahamaphilip@yahoo.com>
2016-12-12 11:35:41 -0400

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -15,35 +15,108 @@ public class Cliente {
     private String data_de_nascimento;
     
     Scanner input = new Scanner(System.in);
-    //Metódo para adicionar clientes
+    
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationalidade() {
+        return nationalidade;
+    }
+
+    public void setNationalidade(String nationalidade) {
+        this.nationalidade = nationalidade;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getPassaporte() {
+        return passaporte;
+    }
+
+    public void setPassaporte(String passaporte) {
+        this.passaporte = passaporte;
+    }
+
+    public String getData_de_nascimento() {
+        return data_de_nascimento;
+    }
+
+    public void setData_de_nascimento(String data_de_nascimento) {
+        this.data_de_nascimento = data_de_nascimento;
+    }
+   //Metodo para inserir dados do cliente 
     public void insereCliente(){
         System.out.print("Nome: ");
-        this.nome = input.nextLine();
+        this.setNome(input.nextLine());
         
         System.out.print("Endereço: ");
-        this.endereco = input.nextLine();
+        this.setEndereco(input.nextLine());
         
         System.out.print("Telefone: ");
-        this.contato = input.nextLine();
+        this.setContato(input.nextLine());
         
         System.out.print("E-mail: ");
-        this.email = input.nextLine();
+        this.setEmail(input.nextLine());
         
         System.out.print("Nationalidade: ");
-        this.nationalidade = input.nextLine();
+        this.setNationalidade(input.nextLine());
         
-        if((this.nationalidade).equals("Br")){//Estrangeiros só precisarão informar o número de passaporte
+        if((this.nationalidade).equals("Br")){
             System.out.print("RG: ");
-            this.RG = input.nextLine();
+            this.setRG(input.nextLine());
             
             System.out.print("CPF: ");
-            this.CPF = input.nextLine();
+            this.setCPF(input.nextLine());
         }
         else{
             System.out.print("Passaporte: ");
-            this.passaporte = input.nextLine();
+            this.setPassaporte(input.nextLine());
         }
         System.out.print("Data De Nascimento(dd/mm/yyyy): ");
-        this.data_de_nascimento = input.nextLine();
+        this.setData_de_nascimento(input.nextLine());
     }
 }
