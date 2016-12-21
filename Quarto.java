a64f1e1e9881b74853c089f489cde76b12ec38b2
felipey2010 <mahamaphilip@yahoo.com>
2016-12-21 11:27:40 -0400

    Update Quarto.java

--- Quarto.java
+++ Quarto.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemadehotel;
 
-/**
- *
- * @author FELIPEY
- */
 public class Quarto {
-    int NumQuartos;
+    private int numQuartos;
+    private String situacaoQuarto;
+    private String disponibilidadeQuarto;
+
+    public int getNumQuartos() {
+        return numQuartos;
+    }
+
+    public void setNumQuartos(int numQuartos) {
+        this.numQuartos = numQuartos;
+    }
+
+    public String getSituacaoQuarto() {
+        return situacaoQuarto;
+    }
+
+    public void setSituacaoQuarto(String situacaoQuarto) {
+        this.situacaoQuarto = situacaoQuarto;
+    }
+
+    public String getDisponibilidadeQuarto() {
+        return disponibilidadeQuarto;
+    }
+
+    public void setDisponibilidadeQuarto(String disponibilidadeQuarto) {
+        this.disponibilidadeQuarto = disponibilidadeQuarto;
+    }
 }
