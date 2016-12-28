f59f0bbb76fe67512d5ae21a399bc2ee12285358
pires28 <junior-pires.rr@hotmail.com>
2016-12-28 19:42:41 -0200

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -3,21 +3,12 @@ package sistemadehotel;
 public class Hotel {
 
     public static void main(String[] agrs){
-		Funcionario gerente = new Gerente();
-		gerente.setNome("Mario");
-		gerente.setLogin("Ana");
-		gerente.setSenha("ABCDEFGHIJKLMNOPQRSTUVX");
-    try {
-			gerente.autenticaLogin(gerente.getLogin());
-    }catch (Exception e) {
-			System.out.println(e.getMessage());
-    }
-	try {
-		gerente.autenticaSenha(gerente.getSenha());
-	}catch (Exception e){
-		System.out.println(e.getMessage());
-	}
-
-    }
+       Funcionario gerente = new Gerente();
+       ((Gerente)gerente).recepcionista = new Recepcionista[2];
+       int i , n;
+       System.out.println("*** BEM VINDO AO SISTEMA ***");
+       System.out.println("1 - GERENCIA    2 - RECEPÇÃO");
+       
+        
 
 }
