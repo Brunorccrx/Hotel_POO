414c7dc824e1eb3d34ea3a4c0f020b462e9c870d
Brunorccrx <brunorccrx@gmail.com>
2016-12-28 15:38:10 -0400

    Update classse recepcionista

--- Recepcionista.java
+++ Recepcionista.java
@@ -3,15 +3,34 @@ package sistemadehotel;
 classe Recepcionista extends Funcionario implements Autentica{
 
 
-	private String login;
-	private String senha;
+private String login;
+private String senha;
 
-	public String getSenha() {
-			return senha;
-	}
 
-	public void setSenha(String senha) {
-			this.senha = senha;
-	}
+public String getSenha() {
+		return senha;
+}
+
+public void setSenha(String senha)throws IOException{
+	if(senha.length()<7)
+		throw new IOException("Senha deve ter no mínimo 7 caracteres");
+	if(senha.length()>14)
+		throw new IOException("Senha ultrapassou o limite máximo de 14 caracteres");
+
+		this.senha = senha;
+}
+
+public String getLogin() {
+		return login;
+}
+
+public void setLogin(String login) {
+	if(login.length()<5)
+		throw new IOException("Login deve ter no mínimo 5 caracteres");
+	if(login.length()>20)
+		throw new IOException("Login ultrapassou o limite máximo de 20 caracteres");
+
+		this.login = login;
+}
 
 }
