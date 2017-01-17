ef2c55ab181ff6c10cc3a3dd768e21b97a2ccaff
Gabriel Rodrigues Machado <grmachado12@hotmail.com>
2017-01-17 12:02:08 -0400

    Update Recepcionista.java

--- Recepcionista.java
+++ Recepcionista.java
@@ -2,8 +2,6 @@ package sistemadehotel;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Recepcionista extends Funcionario implements Autentica {
 	
@@ -11,7 +9,6 @@ public class Recepcionista extends Funcionario implements Autentica {
 	private String senha;
 	private Cliente cliente;
 	
-
 	public String getSenha() {
 		return senha;
 	}
@@ -68,39 +65,5 @@ public class Recepcionista extends Funcionario implements Autentica {
 		}
 		
 		return cliente;
-	}
-public class Recepcionista extends Funcionario implements Autentica{
-	
-	private String login;
-	private String senha;
-	
-	public String getSenha() {
-    return senha;
-	}
-	public void setSenha(String senha)throws IOException{
-    if(senha.length()<7){
-	throw new IOException("Senha deve ter no mínimo 7 caracteres");
-    }
-    if(senha.length()>14){
-	throw new IOException("Senha ultrapassou o limite máximo de 14 caracteres");
-    }
-
-	this.senha = senha;
-}
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) throws IOException {
-		if(login.length()<5){
-			throw new IOException("Login deve ter no mínimo 5 caracteres");
-		}
-	if(login.length()>20){
-		throw new IOException("Login ultrapassou o limite máximo de 20 caracteres");  
-	}	
-	this.login = login;	
-}
-	@Override
-	public boolean autentica(String senha){
-		return this.senha.equals(senha);
 	}
 }
