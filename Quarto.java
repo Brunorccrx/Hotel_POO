d531bb5fe54d8538eafc690d4c2319f2a34f7354
Brunorccrx <brunorccrx@gmail.com>
2017-01-20 15:46:38 -0400

    Add files via upload

--- Quarto.java
+++ Quarto.java
@@ -1,49 +1,77 @@
 
-package sistemadehotel;
 
 public class Quarto {
 	static private int totalQuartos;
   private String numeroQuarto;
-  private String disponibilidadeQuarto;
-	private String situacaoQuarto;
+  private String situacaoQuarto;
 	private String tipoQuarto;
+	private double consumo;
 	Cliente cliente;
 
 
-	public String getTipoQuarto(){
-		return this.numeroQuarto;
+//Construtor
+	public Quarto(String numeroQuarto,String tipoQuarto){
+		Quarto.totalQuartos++;
+		this.numeroQuarto=numeroQuarto;
+		this.tipoQuarto=tipoQuarto;
 	}
-	public String setTipoQuarto(String tipoQuarto){
+
+//Get and Set
+	public double getConsumo(){
+		return this.consumo;
+	}
+	public void setConsumo(double consumo){
+		this.consumo=consumo;
+	}
+	public String getTipoQuarto(){
+		return this.tipoQuarto;
+	}
+	public void setTipoQuarto(String tipoQuarto){
 		this.tipoQuarto=tipoQuarto;
 	}
 	public String getNumeroQuarto(){
 		return this.numeroQuarto;
 	}
-	public String setNumeroQuarto(int numeroQuarto){
+	public void setNumeroQuarto(String numeroQuarto){
 		this.numeroQuarto=numeroQuarto;
 	}
-
   public int getTotalQuartos() {
     return totalQuartos;
   }
-
   public void setTotalQuartos(int totalQuartos) {
-    this.totalQuartos = totalQuartos;
+    Quarto.totalQuartos = totalQuartos;
   }
-
-  public String getDisponibilidadeQuarto() {
-    return disponibilidadeQuarto;
-  }
-
-  public void setDisponibilidadeQuarto(String disponibilidadeQuarto) {
-    this.disponibilidadeQuarto = disponibilidadeQuarto;
-  }
-
- 	public String getSituacaoQuarto() {
+  public String getSituacaoQuarto() {
     return situacaoQuarto;
   }
+	public void setSituacaoQuarto(String situacaoQuarto){
+		this.situacaoQuarto=situacaoQuarto;
+	}
 
-  public void setSituacaoQuarto(String situacaoQuarto) {
-    this.situacaoQuarto = situacaoQuarto;
-  }
+//Funcoes Quarto
+	public double valorQuarto(){
+		if(this.tipoQuarto.equals("Solteiro"))
+			return 1000;
+		else if(this.tipoQuarto.equals("Casal"))
+			return 2000;
+		else
+			return 1500;//DuploSolteiro
+	}
+	public String quartoDisponivel(Cliente cliente){
+		if(cliente==null && situacaoQuarto.equals("Limpo"))
+			return "Quarto disponivel.";
+		else if(cliente!=null)
+			return "Quarto indisponivel.Quarto ocupado pelo(a) cliente"+" "+cliente.getNome();
+		else
+			return "Quarto indisponivel.Quarto ainda não foi limpo.";
+	}
+	public void removerCliente(){
+		this.cliente = null;
+	}
+	public void adicionarCliente(Cliente cliente){
+		this.cliente=cliente;
+		this.situacaoQuarto="Ocupado";
+	}
+	//Funcao para calucular consumo
+	
 }
