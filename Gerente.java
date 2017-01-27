5ec8b2bd9414267764aaf8116c8fe34ee59fd711
pires28 <junior-pires.rr@hotmail.com>
2017-01-27 17:44:25 -0200

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,5 +1,8 @@
 package sistemadehotel;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Gerente extends Funcionario implements Autentica {
@@ -8,36 +11,63 @@ public class Gerente extends Funcionario implements Autentica {
 
     private String login;
     private String senha;
-    Camareira[] camareira;
-    Recepcionista[] recepcionista;
-    static int contador1 = 0;
-    static int contador2 = 0;
-
-    public void cadastrarRecepcionista(Recepcionista f) {
-        this.recepcionista[contador1] = f;
-        contador1++;
-    }
-    public void mostraRecepcionista() {
-        for(int i = 0 ; i < this.recepcionista.length ;i++){
-            System.out.println("Nome: " + recepcionista[i].getNome() + "\nCPF: "
-            + recepcionista[i].getCPF() + "\nData de nascimento: "+ recepcionista[i].getDataNasc()
-            + "\nSalario: " + recepcionista[i].getSalario());
-            System.out.println();
+    
+    File arquivo;
+    public void cadastrarFuncionario(Funcionario f ,String nome , String cpf){
+        arquivo = new File("Banco de Dados/"+nome+cpf+".txt");
+        try{
+            arquivo.createNewFile();
+            
+            FileWriter fw = new FileWriter(arquivo);
+            BufferedWriter bw = new BufferedWriter(fw);
+            
+            bw.write("Nome: "+f.getNome());
+            bw.newLine();
+            bw.write("RG: "+ f.getRG());
+            bw.newLine();
+            bw.write("CPF: "+ f.getCPF());
+            bw.newLine();
+            bw.write("Data de nascimento: "+ f.getDataNasc());
+            bw.newLine();
+            bw.write("Contato: "+ f.getNumTelefone());
+            bw.newLine();
+            bw.write("RUA: "+ f.getRua());
+            bw.newLine();
+            bw.write("Bairro: "+ f.getBairro());
+            bw.newLine();
+            bw.write("Complemento: "+ f.getComplemento());
+            bw.newLine();
+            bw.write("Numero casa: "+ f.getNumeroCasa());
+            bw.newLine();
+            bw.write("Cidade: "+ f.getCidade());
+            bw.newLine();
+            bw.write("Estado: "+ f.getEstado());
+            bw.newLine();
+            bw.write("Salario: "+ f.getSalario());
+            bw.newLine();
+            
+            bw.close();
+            fw.close();
+            
+        }catch(IOException ex){
+            
         }
     }
-    public void cadastrarCamareira(Camareira f){
-        this.camareira[contador2] = f;
-        contador2++;
-    }
-    public void mostraCamareira() {
-        for(int i = 0 ; i < this.camareira.length ;i++){
-            System.out.println("Nome: " + camareira[i].getNome() + "\nCPF: "
-            + camareira[i].getCPF() + "\nData de nascimento: "+ camareira[i].getDataNasc()
-            + "\nSalario: " + camareira[i].getSalario());
-            System.out.println();
+    public boolean verificaFuncionario(String nome, String cpf){
+        arquivo = new File("Banco de Dados/"+nome+cpf+".txt");
+        if(arquivo.exists()){
+            System.out.println("Funcionario encontrado!");
+            return true;
+        }else{
+            System.out.println("Funcionario nao encontrado!");
+            return false;
         }
+        
     }
-
+    public void removerFuncionario(String nome,String cpf){
+        arquivo = new File("Banco de Dados/"+nome+cpf+".txt");
+        arquivo.delete();
+    }
     public String getSenha() {
         return senha;
     }
