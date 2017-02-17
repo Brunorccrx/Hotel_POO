11072c03d693c8e17f763e0886b72cd7a6a2b707
Gabriel Rodrigues Machado <grmachado12@hotmail.com>
2017-02-17 10:12:56 -0400

    Delete Autentica.java

--- Autentica.java
+++
@@ -1,7 +0,0 @@
-package com.compilar.testes;
-
-public interface Autentica{
-  public boolean autentica(String senha, String login);
-}
-
-//TODO implementar tratamento de erros para set de login e senha como default
