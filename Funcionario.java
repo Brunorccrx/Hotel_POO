3755157645525664cfdd0e7bdbc8365e389604d8
felipey2010 <mahamaphilip@yahoo.com>
2017-03-01 20:02:15 -0400

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -10,23 +10,38 @@ package sistemadehotel;
 abstract public class Funcionario extends Pessoa {
 	private double salario;
 	static private int ID;
-
+	/**
+	*
+	* 
+	*/
 	public Funcionario() {
 		Funcionario.ID++;
 	}
-
+	/**
+	*
+	* @return
+	*/
 	public double getSalario() {
 		return salario;
 	}
-
+	/**
+	*
+	* @param salario
+	*/
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-
+	/**
+	*
+	* @return
+	*/
 	public static int getID() {
 		return ID;
 	}
-
+	/**
+	*
+	* @return
+	*/
 	@Override
 	public String toString() {
 		String saida = "Nome: " + this.getNome() + "\n" + "Data de nascimento: : " + this.getDataNasc() + "\n"
